0acf9e4f3073a779285082b87d2ec556c1067c55
sc-tarek <tarek.ghanoum@dk.ey.com>
2023-01-30 10:31:28 +0100

    fetc stof

--- testing/user_input.py
+++ testing/user_input.py
@@ -1,22 +1,23 @@
 import sys
 import bosdyn.client
+from bosdyn.client.robot_command import RobotCommandClient, blocking_stand, blocking_sit
 
-
+# ---------------------
 sdk = bosdyn.client.create_standard_sdk('understanding-spot')
 robot = sdk.create_robot('192.168.80.3')
 robot.authenticate('user', 'hhe262iz9zez')
 
+# ---------------------
 lease_client = robot.ensure_client('lease')
 #lease = lease_client.return_lease(lease)
 lease = lease_client.acquire()
 lease_keep_alive = bosdyn.client.lease.LeaseKeepAlive(lease_client)
 
+# ---------------------
 robot.power_on(timeout_sec=20)
-robot.is_powered_on()
-
 robot.time_sync.wait_for_sync()
 
-from bosdyn.client.robot_command import RobotCommandClient, blocking_stand, blocking_sit, blocking_selfright
+# ---------------------
 command_client = robot.ensure_client(RobotCommandClient.default_service_name)
 
 while True:
