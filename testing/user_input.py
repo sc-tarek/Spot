4e3a73dd4cffe97c732e4476e9d091772317fb32
sc-tarek <tarek.ghanoum@dk.ey.com>
2023-01-30 13:36:40 +0100

    update fetch

--- testing/user_input.py
+++ testing/user_input.py
@@ -8,10 +8,10 @@ robot = sdk.create_robot('192.168.80.3')
 robot.authenticate('user', 'hhe262iz9zez')
 
 # ---------------------
-#lease_client = robot.ensure_client('lease')
+lease_client = robot.ensure_client('lease')
 ##lease = lease_client.return_lease(lease)
-#lease = lease_client.acquire()
-#lease_keep_alive = bosdyn.client.lease.LeaseKeepAlive(lease_client)
+lease = lease_client.acquire()
+lease_keep_alive = bosdyn.client.lease.LeaseKeepAlive(lease_client)
 
 # ---------------------
 robot.power_on(timeout_sec=20)
@@ -24,6 +24,7 @@ while True:
     user_input = input("Enter W to stand, S to sit or 'Q' to quit: ")
 
     if user_input.upper() == 'Q':
+        lease_client.return_lease(lease)
         break
     elif user_input.upper() == 'W':
         blocking_stand(command_client, timeout_sec=10)
